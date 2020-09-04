0621b900374a517ebd77421ef49bd64f2609c0b6
Big Tobs <58257308+GiveMeTobleroneeeessreeeee@users.noreply.github.com>
2020-09-04 10:57:46 +0200

    two servers

--- EchoServer/Server.cs
+++ EchoServer/Server.cs
@@ -1,10 +1,76 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EchoServer
 {
-    class Server
+    /// <summary>
+    /// Eksemplet fra https://docs.microsoft.com/en-us/dotnet/api/system.net.sockets.tcplistener?redirectedfrom=MSDN&view=netcore-3.1
+    /// </summary>
+    public static class Server
     {
+        public static void Start()
+        {
+            //IP = IPAddress.Loopback
+            //Port = 7 eller 7777 (port 7 er standard for echo server)
+            TcpListener server = null;
+            try
+            {
+                Int32 port = 7777;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+                server = new TcpListener(localAddr, port);
+
+                server.Start();
+
+                Byte[] bytes = new byte[256];
+                String data = null;
+
+                while (true)
+                {
+                    Console.Write("Waiting for a connection...");
+
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    data = null;
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+
+                        data = data.ToUpper();
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Sent: {0}", data);
+                    }
+
+                    client.Close();
+                }
+
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+                throw;
+            }
+            finally
+            {
+                server.Stop();
+                Console.WriteLine("/nHit enter to continue...");
+                Console.Read();
+            }
+
+        }
+
     }
 }
---
+++ EchoServer/TServer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EchoServer
+{
+    /// <summary>
+    /// Jamshid's eksempel
+    /// </summary>
+    public static class TServer
+    {
+        public static void Start()
+        {
+
+            TcpListener serverSocket = new TcpListener(IPAddress.Loopback, 7);
+            serverSocket.Start();
+
+            TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+            Console.WriteLine("server activated");
+
+            Stream ns = connectionSocket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true;
+
+            string message = sr.ReadLine();
+
+            Console.WriteLine("received message" + message);
+            if (message != null)
+                sw.WriteLine(message.ToUpper());
+            ns.Close();
+            Console.WriteLine("net stream closed");
+
+            connectionSocket.Close();
+            Console.WriteLine("connection socket closed");
+            serverSocket.Stop();
+            Console.WriteLine("server stopped");
+
+        }
+    }
+}
